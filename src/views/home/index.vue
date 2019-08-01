5c08df954625453f38eb07afb1142ab934f0ccc0
耿源 <2788816975@qq.com>
2019-08-01 16:26:16 +0800

    项目初始化 / Vant组件测试

---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home-container">
+        <van-button type="default">默认按钮</van-button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
